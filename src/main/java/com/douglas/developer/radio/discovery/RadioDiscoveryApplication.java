14bb7a5b217c80a498041f5a1e71149dc9741d3e
Douglas Ribeiro <douglas.ribeiro.id@gmail.com>
2023-12-20 16:55:49 -0300

    Projeto criado

---
+++ src/main/java/com/douglas/developer/radio/discovery/RadioDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package com.douglas.developer.radio.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RadioDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadioDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/douglas/developer/radio/discovery/RadioDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglas.developer.radio.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadioDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
